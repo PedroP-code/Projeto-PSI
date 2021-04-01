fe3dfd98c02e0fcfb5f1015242f39377cc30164e
Pedro Paulo <pedropaulolucas@hotmail.com>
2021-04-01 11:58:25 -0300

    framework ASP.NET Identity implemented

---
+++ WebApplicationTII/Areas/Tabelas/Controllers/CategoriasController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Modelo.Tabelas;
+using Servico.Tabelas;
+
+namespace WebApplicationTII.Areas.Tabelas.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        //private EFContext context = new EFContext();
+        private CategoriaServico categoriaServico = new CategoriaServico();
+        // GET: Categoria
+        public ActionResult Index()
+        {
+            return View(categoriaServico.ObterCategoriasClassificadasPorNome());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Categoria categoria)
+        {
+            return GravarCategoria(categoria);
+        }
+        public ActionResult Edit(long? id)
+        {
+            return ObterVisaoCategoriaPorId(id);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Categoria categoria)
+        {
+            return GravarCategoria(categoria);
+        }
+
+        public ActionResult Details(long? id)
+        {
+            return ObterVisaoCategoriaPorId(id);
+        }
+
+        public ActionResult Delete(long? id)
+        {
+            return ObterVisaoCategoriaPorId(id);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(long id)
+        {
+            try
+            {
+                Categoria categoria = categoriaServico.EliminarCategoriaPorId(id);
+                TempData["Message"] = "Categoria " + categoria.Nome.ToUpper() + " foi removido";
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        private ActionResult ObterVisaoCategoriaPorId(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categoria categoria = categoriaServico.ObterCategoriaPorId((long)id);
+            if (categoria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categoria);
+        }
+
+        private ActionResult GravarCategoria(Categoria categoria)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    categoriaServico.GravarCategoria(categoria);
+                    return RedirectToAction("Index");
+                }
+                return View(categoria);
+            }
+            catch
+            {
+                return View(categoria);
+            }
+        }
+    }
+}
