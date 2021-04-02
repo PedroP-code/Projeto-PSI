890fa69256485c3b849bf46e837ccc5bdb8ad1b6
Pedro Paulo <pedropaulolucas@hotmail.com>
2021-04-02 01:03:08 -0300

    Create/Edit/Delete Usuario

---
+++ WebApplicationTII/DAL/IdentityDbContextAplicacao.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebApplicationTII.Areas.Seguranca.Data;
+
+namespace WebApplicationTII.DAL
+{
+    public class IdentityDbContextAplicacao : IdentityDbContext<Usuario>
+    {
+        public IdentityDbContextAplicacao() : base("IdentityDb")
+        { }
+        static IdentityDbContextAplicacao()
+        {
+            Database.SetInitializer<IdentityDbContextAplicacao>(
+            new IdentityDbInit());
+        }
+        public static IdentityDbContextAplicacao Create()
+        {
+            return new IdentityDbContextAplicacao();
+        }
+        }
+        public class IdentityDbInit : DropCreateDatabaseIfModelChanges<IdentityDbContextAplicacao>
+        {
+        }
+}
